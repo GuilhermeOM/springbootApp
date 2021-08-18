eabd53c67d5d9737b75a69264167718b487a730e
GuilhermeOM <guilhermeomalta0@gmail.com>
2021-08-17 21:28:55 -0300

    Associating Products and Categories

--- src/main/java/com/example/springProject/config/TestConfig.java
+++ src/main/java/com/example/springProject/config/TestConfig.java
@@ -49,6 +49,15 @@ public class TestConfig implements CommandLineRunner{
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
         
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+        
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+        
         User u1 = new User(null, "Test1", "test1@gmail.com", "12345-1", "test1&123");
         User u2 = new User(null, "Test2", "test2@gmail.com", "12345-2", "test2&123");
         
